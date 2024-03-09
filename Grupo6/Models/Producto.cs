dc8f01fab318828c9f09fd190e285d3b048fce2e
Erick J <53590767+Rainfire267@users.noreply.github.com>
2024-03-09 09:57:17 -0600

    Version 1Agregar archivos de proyecto.

---
+++ Grupo6/Models/Producto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grupo6.Models
+{
+    public class Producto
+    {
+        public int ID_Producto { get; set; }
+
+        public string Nombre_Producto { get; set; }
+
+        public DateTime FechaIngreso{ get; set; }
+
+        public DateTime FechaEgreso { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public Ubicacion Ubicacion { get; set; }
+
+        public Estado_productos Estado { get; set; }
+    }
+
+
+    public class Ubicacion
+    {
+        public int Id_Ubicacion { get; set; }
+
+        public string Nombre_Ubicacion { get; set; }
+
+
+    }
+
+    public class Estado_productos
+    {
+        public int ID_Estado { get; set; }
+
+        public string Nombre_estado { get; set; }
+    }
+
+}
